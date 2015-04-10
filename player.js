1e5f2aef58c00aaa8b6b14edf5d159a1d1666d1c
tomekosinski <tomekosinski@hotmail.com>
2015-04-10 12:08:52 +0100

    Tomeks changes

--- player.js
+++ player.js
@@ -1,31 +1,26 @@
+//Tomek task
 
 (function PLAYER() {
-
 	var Player = function(name, credit) {
 		this.name = name;
-		if (credit) { 
-			this.credit = credit;
-		}
-		else {
-			this.credit = "100";
-		}
-		console.log('Created a player '+ this.name);
+			if (credit) { 
+				var credit = credit;
+			}
+			else {
+				var credit = 100;
+			}
+		 this.showCredit = function(){
+       	 return credit;
+   		 };
+		this.updateCredit = function(newCredit) {
+		credit = newCredit;
+		};
+		console.log('Created a player '+ this.name +' with a credit of ' +credit);
 	};
-
 	Player.prototype.changeName = function(newName) {
 		this.forename = newName;
 	};
-	
-	Player.prototype.updateCredit = function(newCredit) {
-		this.credit = newCredit;
-	};
-	
-	Player.prototype.showCredit = function() {
-		console.log("The credit is:"+ this.credit);
-	};
-
 	window.Player = Player;
-
 })();
 
 var player1 = new Player('Tomek');
@@ -34,6 +29,6 @@ var player3 = new Player('Carl');
 
 player1.updateCredit('200');
 
-player1.showCredit();
-player2.showCredit();
-player3.showCredit();
+console.log(player1.showCredit());
+console.log(player2.showCredit());
+console.log(player3.showCredit());
